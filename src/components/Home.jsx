0cf5967231400dee5284393a6ebe6efa908ff908
lfwzk <fwzk777@gmail.com>
2023-09-03 19:26:53 -0400

    beta 0.0.1

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Header } from './Header'
+import { Footer } from './Footer'
+import { Hero } from './Hero'
+import { Support } from './Support'
+import { Promo } from './Promo'
+import { Features } from './Features'
+
+export const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Support/>
+    <Promo/>
+    <Features/>
+    <Footer/>
+
+    </>
+  )
+}
---
+++ src/components/Store.jsx
@@ -0,0 +1,171 @@
+import React from 'react'
+import { Header } from './Header'
+import { Footer } from './Footer'
+
+export const Store = () => {
+  return (
+    <>
+    <Header/>
+    <section className="py-6 dark:bg-gray-800">
+	<div className="container flex flex-col justify-center p-4 mx-auto">
+		<div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+			 {/* -------------------------------------------------------- */}
+
+       <div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-auto h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+	 {/* -------------------------------------------------------- */}
+   <div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-full sm:w-80 bg-base-100 shadow-xl p-4">
+  <figure className="w-80 h-60 sm:w-full sm:h-80 flex items-center justify-center">
+    <img src="https://images.pexels.com/photos/2148216/pexels-photo-2148216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Shoes" className="square-image" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Computadora
+      <div className="badge badge-secondary">nuevo</div>
+    </h2>
+    <p className="text-lg">BoB. PRECIO</p>
+    <div className="card-actions justify-end">
+      <button className="btn bg-red-500 text-white">Comprar</button>
+    </div>
+  </div>
+</div>
+
+
+
+  
+
+
+		</div>
+	</div>
+</section>
+    <Footer/>
+    </>
+  )
+}
+ 
