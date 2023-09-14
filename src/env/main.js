485666259f38a4a333b34e67c657d6951c699212
lfwzk <fwzk777@gmail.com>
2023-09-14 09:31:24 -0400

    seo and change route strapi

--- src/env/main.js
+++ src/env/main.js
@@ -1,5 +1,5 @@
 const enviroment = {
-  api: "http://192.241.143.182:1337/api",
-  strapi:"http://192.241.143.182:1337"
+  api: "http://3.88.48.69:1337/api",
+  strapi: "http://3.88.48.69:1337",
 };
 export default enviroment;
